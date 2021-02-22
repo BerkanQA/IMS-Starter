6e2dc9bb8c780073d49063f5b22ceb0ec72c9067
Your Name <you@example.com>
2021-02-22 14:15:55 +0000

    Order.java has create but there are some errors

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -5,8 +5,92 @@ public class Order {
 		private Long orderID;
 		private Long customerID;
 		private Long itemID;
-		private int quantity;
+		public int quantity;
+
+		public Order(int quantity) {
+			this.setQuantity(quantity);
+			
+		}
 		
-		public Order()
+		public Order(Long orderID, Long customerID, Long itemID, int quantity) {
+			this.setOrderID(orderID);
+			this.setCustomerID(customerID);
+			this.setItemID(itemID);
+			this.setQuantity(quantity);
+		}
+		
+		public Long getOrderID() {
+			return orderID;
+			
+		}
+		
+		public void setOrderID(Long orderID) {
+			this.orderID = orderID;
+		}
+		
+		public Long getCustomerID() {
+			return customerID;
+		}
+		
+		public void setCustomerID(Long customerID) {
+			this.customerID = customerID;
+		}
+		public Long getItemID() {
+			return itemID;
+		}
+		
+		public void setItemID(Long itemID) {
+			this.itemID = itemID;
+		}
+		
+		@Override
+		public String toString() {
+			return "Order:" + orderID + "Customer ID:" + customerID + "Item ID:" + itemID + "Quantity:" + quantity; 
+			
+		}
+		
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+
+			result = prime * result + ((orderID == null) ? 0 : orderID.hashCode());
+			result = prime * result + ((customerID == null) ? 0 : customerID.hashCode());
+			result = prime * result + ((itemID == null) ? 0 : itemID.hashCode());
+			result = prime * result + ((quantity == null) ? 0 : quantity.hashCode());
+			return result;
+		}
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Order other = (Order) obj;
+			if (getCustomerID() == null) {
+				if (other.getCustomerID() != null)
+					return false;
+			} else if (!getCustomerID().equals(other.getCustomerID()))
+				return false;
+			if (orderID == null) {
+				if (other.orderID != null)
+					return false;
+			} else if (!orderID.equals(other.getOrderID()))
+				return false;
+			if (itemID == null) {
+				if (other.itemID != null)
+					return false;
+			} else if (!itemID.equals(other.itemID))
+				return false;
+			if (quantity == null) {
+				if (other.quantity != null)
+					return false;
+			} else if (!quantity.equals(other.quantity))
+				return false;
+			return true;
+		}
+		
+		
 		
 }
